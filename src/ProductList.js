2717576955b57d09911d2e22da77d4c8f2ec34b9
Matthew Mogavero <matt.moga@gmail.com>
2018-03-08 20:33:13 -0500

    connected the product component and provided the ability to dispatch add item and remove item actions

--- src/ProductList.js
+++ src/ProductList.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Product from './Product';
 
 const ProductList = props => {
-    const ProductItems = props.products.map((product, index) => <Product {...product} key={index}/>);
+    const ProductItems = props.products.map((product, index) => (
+    <Product {...product} key={index} position={index} />
+));
     return (
         <ul>
             {ProductItems}
